1523bbacd97baf1d2b496a7a6354752e4077e091
cindyyzhu <cindyzhu8207@gmail.com>
2024-02-17 22:20:27 -0500

    updated files

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
+//global variables
+var likedPost = false;
+var likes = 8;
+var copiedPost = false;
+
+var likedPost2 = false;
+var likes2 = 11;
+var copiedPost2 = false;
+
 //continuous animation loops
 timedLoop(1000, function() {
   hideElement("authenticatingHeading");
@@ -46,3 +55,129 @@ onEvent("backButton", "click", function( ) {
 onEvent("backButton2", "click", function( ) {
   setScreen("selectRegion");
 });
+onEvent("heart", "click", function( ) {
+  if (likedPost == false) {
+    likedPost = true;
+    showElement("greenHeart");
+    likes ++;
+    setText("likesNumber", likes);
+  } else if (likedPost == true) {
+    hideElement("greenHeart");
+    likedPost = false;
+    likes --;
+    setText("likesNumber", likes);
+  }
+});
+onEvent("copyButton", "click", function( ) {
+  if (copiedPost == false) {
+    copiedPost = true;
+    showElement("copiedButton");
+  }
+});
+onEvent("shareButton", "click", function ( ) {
+  console.log("hi");
+  showElement("sharePopUp");
+  showElement("exitPopUp");
+  showElement("copyButton");
+  if (copiedPost == true) {
+    showElement("copiedButton");
+  }
+});
+onEvent("exitPopUp", "click", function ( ) {
+  hideElement("sharePopUp");
+  hideElement("exitPopUp");
+  hideElement("copyButton");
+  hideElement("copiedButton");
+});
+onEvent("backButton3", "click", function( ) {
+  setScreen("communityToronto");
+});
+onEvent("uploadPhoto", "click", function( ) {
+  open("https://www.google.com/imghp");
+});
+onEvent("communityTorontoSearch", "click", function( ) {
+  setScreen("searchScreen");
+});
+onEvent("ecoVaderSearchButton", "click", function( ) {
+  var searchInput = getProperty("ecoSearchInput", "value");
+  console.log(searchInput);
+  if (searchInput == "") {
+    setScreen("searchScreen");
+  } else {
+    showElement("searchLoadingGIF");
+    hideElement("ecoVaderSearchButton");
+    setTimeout(function() {
+      if (searchInput == "Garlic Mustard" || searchInput == "garlic mustard" || searchInput == "Garlic mustard") {
+        setScreen("garlicMustardSearchResults");
+        setText("garlicMustardSearchInput", searchInput);
+        setText("showingResultsGM", "Showing results for '" + searchInput + "'");
+        showElement("ecoVaderSearchButton");
+        hideElement("searchLoadingGIF");
+      } else if (searchInput == "tellima grandiflora" || searchInput == "Tellima Grandiflora" || searchInput == "tellima grandiflora") {
+        setScreen("grandifloraSearchResults");
+        setText("grandifloraSearchInput", searchInput);
+        setText("showingResultsTG", "Showing results for '" + searchInput + "'");
+        showElement("ecoVaderSearchButton");
+        hideElement("searchLoadingGIF");
+      } else {
+        setScreen("searchResults");
+        setText("searchResultsInput", searchInput);
+        showElement("ecoVaderSearchButton");
+        hideElement("searchLoadingGIF");
+      }
+    }, 5000);
+  }
+  createRecord("userSearchHistory", {searches:searchInput}, function() {
+    setTimeout(function() {
+      setProperty("ecoSearchInput", "text", "");
+    }, 5000);
+  });
+});
+onEvent("backButton4", "click", function( ) {
+  setScreen("searchScreen");
+});
+onEvent("backButton5", "click", function( ) {
+  setScreen("searchScreen");
+});
+onEvent("backButton6", "click", function( ) {
+  setScreen("searchScreen");
+});
+onEvent("backButton7", "click", function( ) {
+  setScreen("communityToronto");
+});
+onEvent("nowInTorontoButton", "click", function( ) {
+  setScreen("nowInToronto");
+});
+
+//----
+
+onEvent("heart", "click", function( ) {
+  if (likedPost == false) {
+    likedPost = true;
+    showElement("greenHeart");
+    likes ++;
+    setText("likesNumber", likes);
+  } else if (likedPost == true) {
+    hideElement("greenHeart");
+    likedPost = false;
+    likes --;
+    setText("likesNumber", likes);
+  }
+});
+onEvent("copyButton", "click", function( ) {
+  if (copiedPost == false) {
+    copiedPost = true;
+    showElement("copiedButton");
+  }
+});
+onEvent("shareButton", "click", function ( ) {
+  console.log("hi");
+  showElement("sharePopUp");
+  showElement("exitPopUp");
+  showElement("copyButton");
+  if (copiedPost == true) {
+    showElement("copiedButton");
+  }
+});
+
+
